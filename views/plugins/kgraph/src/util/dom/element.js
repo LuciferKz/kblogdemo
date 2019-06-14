fc009e1083d158a0f39ec1bcceb8bf8bd7e79e1a
Kim Zhang <ZHAN712@sh.arvatochina.com>
2019-06-14 17:53:35 +0800

    fix dom show hide

--- views/plugins/kgraph/src/util/dom/element.js
+++ views/plugins/kgraph/src/util/dom/element.js
@@ -1,97 +1,118 @@
-import { addEvent, addWheelEvent } from './event';
-import Util from '../../util';
+import { addEvent, addWheelEvent } from './event'
 
-let KElement = function (dom) {
-  let k = this;
-  k.dom = dom;
+const getCss = function(element, attr) {
+  if (window.getComputedStyle) {
+    return attr
+      ? window.getComputedStyle(element, null)[attr]
+      : window.getComputedStyle(element, null)
+  }
+  return attr ? element.currentStyle[attr] : element.currentStyle
+}
+
+let KElement = function(dom) {
+  let k = this
+  k.dom = dom
 }
 
 KElement.prototype = {
-  append: function (k) {
-    this.dom.appendChild(k.dom);
-    return this;
+  append: function(k) {
+    this.dom.appendChild(k.dom)
+    return this
   },
-  prepend: function (k) {
-    this.dom.insertBefore(k.dom, this.dom.childNodes[0]);
-    return this;
+  prepend: function(k) {
+    this.dom.insertBefore(k.dom, this.dom.childNodes[0])
+    return this
   },
-  insertBefore: function (k) {
-    k.dom.parentNode.insertBefore(this.dom, k.dom);
-    return this;
+  insertBefore: function(k) {
+    k.dom.parentNode.insertBefore(this.dom, k.dom)
+    return this
   },
-  remove: function () {
-    if (!this.dom.parentNode) { return this}
-    this.dom.parentNode.removeChild(this.dom);
-    return this;
+  remove: function() {
+    if (!this.dom.parentNode) {
+      return this
+    }
+    this.dom.parentNode.removeChild(this.dom)
+    return this
   },
-  css: function (name, value) {
-    if (Util.isPlainObject(name)) {
+  css: function(name, value) {
+    if (typeof name === 'object') {
       let styles = name
       for (let style in styles) {
-        this.dom.style[style] = styles[style];
+        this.dom.style[style] = styles[style]
       }
     } else {
-      this.dom.style[name] = value;
+      this.dom.style[name] = value
     }
-    return this;
+    return this
   },
-  addClass: function (cls) {
-    this.dom.classList.add(cls);
-    return this;
+  hasClass: function(cls) {
+    return this.dom.classList.contains(cls)
   },
-  removeClass: function (cls) {
-    this.dom.classList.remove(cls);
-    return this;
+  addClass: function(cls) {
+    this.dom.classList.add(cls)
+    return this
   },
-  show: function () {
-    this.dom.style.display = 'block';
-    return this;
+  removeClass: function(cls) {
+    this.dom.classList.remove(cls)
+    return this
   },
-  hide: function () {
-    this.dom.style.display = 'none';
-    return this;
+  toggleClass: function(cls) {
+    this.hasClass(cls) ? this.removeClass(cls) : this.addClass(cls)
+    return this
   },
-  attrs: function (attrs) {
+  show: function() {
+    this.dom.style.display = this.display
+    this.isHide = false
+    return this
+  },
+  hide: function() {
+    if (this.isHide) return false
+    this.display = getCss(this.dom, 'display') || 'block'
+    this.dom.style.display = 'none'
+    this.isHide = true
+    return this
+  },
+  toggle: function() {
+    this.isHide ? this.show() : this.hide()
+    return this
+  },
+  attrs: function(attrs) {
     for (let name in attrs) {
       this.dom.setAttribute(name, attrs[name])
     }
-    return this;
+    return this
   },
-  props: function (props) {
+  props: function(props) {
     for (let name in props) {
-      this.dom[name] = props[name];
+      this.dom[name] = props[name]
     }
-    return this;
+    return this
   },
-  html: function (text) {
+  html: function(text) {
     if (text || text === '') {
-      this.dom.innerHTML = text;
+      this.dom.innerHTML = text
     } else {
-      return this.dom.innerHTML;
+      return this.dom.innerHTML
     }
-    return this;
+    return this
   },
-  text: function (text) {
+  text: function(text) {
     if (text) {
-      this.dom.textContent = text;
+      this.dom.textContent = text
     } else {
-      return this.dom.textContent;
+      return this.dom.textContent
     }
-    return this;
+    return this
   },
-  on: function (evt, fn, config) {
-    addEvent(this.dom, evt, fn, config);
-    return this;
+  on: function(evt, fn, config) {
+    addEvent(this.dom, evt, fn, config)
+    return this
   },
-  off (evt, fn, config) {
-    this.dom.removeEventListener(evt, fn, config)
-    return this;
+  onWheel: function(fn, config) {
+    addWheelEvent(this.dom, fn, config)
+    return this
   },
-  onWheel: function (fn, config) {
-    addWheelEvent(this.dom, fn, config);
-    return this;
-  },
-  height: function (h) {
+  height: function(h) {
     if (typeof h === 'boolean' && h) {
       return this.dom.offsetHeight
     } else if (typeof h === 'number') {
@@ -100,7 +121,7 @@ KElement.prototype = {
       return this.dom.clientHeight
     }
   },
-  width: function (w) {
+  width: function(w) {
     if (typeof w === 'boolean' && w) {
       return this.dom.offsetWidth
     } else if (typeof w === 'number') {
@@ -108,9 +129,6 @@ KElement.prototype = {
     } else {
       return this.dom.clientWidth
     }
-  },
-  getBoundingClientRect () {
-    return this.dom.getBoundingClientRect()
   }
 }
 
