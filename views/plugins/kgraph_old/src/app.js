9b989214bf6182aa1ce0e72602c430d1c6797150
git <ZHAN712@sh.arvatochina.com>
2019-08-30 13:47:24 +0800

    feat

--- views/plugins/kgraph_old/src/app.js
+++ views/plugins/kgraph_old/src/app.js
@@ -6,7 +6,7 @@ import './css/kgraph.css'
 window.onload = function () {
   console.log(kcomponents)
   const kgraph = new KGraph({
-    container: document.getElementById('kgraph-container'),
+    container: document.getElementById('kgraph-container-1'),
     containerWidth: null,
     containerHeight: function () { return window.innerHeight > 600 ? window.innerHeight - 60 : 540 },
     diagram: {
@@ -49,8 +49,6 @@ window.onload = function () {
         text: 'Start',
         value: 'start',
         iconText: '&#xe6ec;',
-        top: false,
-        left: false,
         onlyone: true,
       },
       {
@@ -58,8 +56,6 @@ window.onload = function () {
         text: 'Start',
         value: 'start',
         iconText: '&#xe6ec;',
-        top: false,
-        left: false,
         onlyone: true,
       },
       {
@@ -67,8 +63,6 @@ window.onload = function () {
         text: 'Start',
         value: 'start',
         iconText: '&#xe6ec;',
-        top: false,
-        left: false,
         onlyone: true,
       },
       {
@@ -76,8 +70,6 @@ window.onload = function () {
         text: 'Start',
         value: 'start',
         iconText: '&#xe6ec;',
-        top: false,
-        left: false,
         onlyone: true,
       },
       {
@@ -85,8 +77,6 @@ window.onload = function () {
         text: 'Start',
         value: 'start',
         iconText: '&#xe6ec;',
-        top: false,
-        left: false,
         onlyone: true,
       },
       {
@@ -94,8 +84,6 @@ window.onload = function () {
         text: 'Start',
         value: 'start',
         iconText: '&#xe6ec;',
-        top: false,
-        left: false,
         onlyone: true,
       },
     ]);
@@ -104,5 +92,95 @@ window.onload = function () {
     // kgraph.set('message', vm.$message);
   
     window.onresize = kgraph.resize;
+  })
+
+  
+  const kgraph2 = new KGraph({
+    container: document.getElementById('kgraph-container-2'),
+    containerWidth: null,
+    containerHeight: function () { return window.innerHeight > 600 ? window.innerHeight - 60 : 540 },
+    diagram: {
+      dragable: false,
+      scroll: true,
+      horizontalAlign: 'center',
+      verticalAlign: 'center',
+      gridWidth: 10,
+      gridLineWidth: 2,
+      gridAlign: true,
+      direction: 'horizontal',
+      contextMenu: true,
+      diagramSize: 'full',
+      diagramWidth: 1002,
+      diagramHeight: 802,
+      // adjustCr: true,
+      offsetX: 240,
+      afterCreatePath: function (path) {
+        pathCount++
+        !path.campaignNodeId && (path.campaignNodeId = 'flow' + (+new Date() + 10 * pathCount ));
+      }
+    },
+    toolbar: {
+      toolsHidden: false,
+    },
+    footer: {
+      hidden: false
+    },
+    sidebar: {
+      hidden: false,
+    }
+  });
+  
+  kgraph2
+  .init()
+  .then(() => {
+    kgraph2.sidebar.createSection('组1', [
+      {
+        key: 'Start',
+        text: 'Start',
+        value: 'start',
+        iconText: '&#xe6ec;',
+        onlyone: true,
+      },
+      {
+        key: 'Start',
+        text: 'Start',
+        value: 'start',
+        iconText: '&#xe6ec;',
+        onlyone: true,
+      },
+      {
+        key: 'Start',
+        text: 'Start',
+        value: 'start',
+        iconText: '&#xe6ec;',
+        onlyone: true,
+      },
+      {
+        key: 'Start',
+        text: 'Start',
+        value: 'start',
+        iconText: '&#xe6ec;',
+        onlyone: true,
+      },
+      {
+        key: 'Start',
+        text: 'Start',
+        value: 'start',
+        iconText: '&#xe6ec;',
+        onlyone: true,
+      },
+      {
+        key: 'Start',
+        text: 'Start',
+        value: 'start',
+        iconText: '&#xe6ec;',
+        onlyone: true,
+      },
+    ]);
+    kgraph2.sidebar.createSection('组2', []);
+  
+    // kgraph.set('message', vm.$message);
+  
+    window.onresize = kgraph2.resize;
   })
 }
