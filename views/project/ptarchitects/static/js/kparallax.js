0f9b8a289c35b1e1c91978f65101e64657efdbf9
zhenkai.zhang <zhenkai.zhangSH@downtown8.com>
2020-12-31 18:22:56 +0800

    fix

--- views/project/ptarchitects/static/js/kparallax.js
+++ views/project/ptarchitects/static/js/kparallax.js
@@ -49,7 +49,7 @@ class KParallax {
         const gamma = e.gamma
         
         const clientX = wratio * (beta + 30)
-        const clientY = hratio * (-gamma - 50)
+        const clientY = hratio * (-gamma + 15)
         const diffX = clientX - center.x 
         const diffY = clientY - center.y
         this.move(diffX, diffY)
