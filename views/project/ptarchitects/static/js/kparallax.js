acafb5f2af38418f63ce887ec7b34640d35b507f
zhenkai.zhang <zhenkai.zhangSH@downtown8.com>
2020-12-31 12:50:21 +0800

    feat

---
+++ views/project/ptarchitects/static/js/kparallax.js
@@ -0,0 +1,57 @@
+class KParallax {
+  constructor (opt) {
+    this.opt = Object.assign({}, {
+      scale: 1.1, // 根据的比例来设置
+      perspective: 2
+    }, opt)
+    this.init()
+  }
+  
+  init () {
+    const layers = document.querySelectorAll('.k-parallax .layer')
+    layers.forEach(l => {
+      l.style.position = 'absolute'
+      l.style.zIndex = (1 - l.dataset.depth).toFixed(2) * 10
+    })
+    this.layers = layers;
+  }
+
+  go () {
+    const opt = this.opt;
+    const layers = this.layers;
+    const cWidth = window.innerWidth;
+    const cHeight = window.innerHeight;
+    const center = { x: cWidth / 2, y: cHeight / 2 };
+    const range = { hor: { max: cWidth / 2, min: cWidth / 2 }, ver: { max: cHeight / 2, min: cHeight / 2 }};
+    const offsetX = (cWidth * opt.scale - cWidth) / 2;
+    const offsetY = (cHeight * opt.scale - cHeight)  / 2;
+    let limit = { t: offsetY, l: offsetX, b: offsetY, r: offsetX };
+    // 深度 影响 焦点移动时 层的移动量
+    document.addEventListener('mousemove', (e) => {
+      const clientX = e.clientX
+      const clientY = e.clientY
+      const diffX = clientX - center.x 
+      const diffY = clientY - center.y
+      let translateX = 0;
+      let translateY = 0;
+      if (diffX > 0) {
+        // 焦点右移
+        translateX = diffX / range.hor.max * limit.l
+      } else {
+        // 焦点左移
+        translateX = diffX / range.hor.min * limit.r
+      }
+
+      if (diffY > 0) {
+        // 焦点下移
+        translateY = diffY / range.hor.max * limit.t
+      } else {
+        // 焦点上移
+        translateY = diffY / range.hor.min * limit.b
+      }
+      layers.forEach(l => {
+        l.style.transform = `translate(${ translateX * (1 + (opt.perspective * (1 - l.dataset.depth))) }px, ${ translateY * (1 + (opt.perspective * (1 - l.dataset.depth))) }px)`
+      })
+    })
+  }
+}
