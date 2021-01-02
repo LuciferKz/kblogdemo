d3f33747a0a19d0b48aa5912925a6887d85d0852
zhenkai.zhang <zhenkai.zhangSH@downtown8.com>
2021-01-02 17:11:33 +0800

    fix

---
+++ views/project/ptarchitects2/static/js/kparallax.js
@@ -0,0 +1,148 @@
+class KParallax {
+  constructor (opt) {
+    this.opt = Object.assign({}, {
+      selector: '',
+      scale: 1.2, // 根据的比例来设置
+      perspective: 5,
+      offsetGamma: 0,
+      offsetBeta: 0,
+      debug: false,
+      // perspective: 1000,
+    }, opt)
+    this.init()
+  }
+  
+  init () {
+    const layers = document.querySelectorAll(`${this.opt.selector} .layer`)
+    layers.forEach(l => {
+      l.style.position = 'absolute'
+      l.style.zIndex = (1 - l.dataset.depth).toFixed(2) * 10
+    })
+    this.layers = layers;
+  }
+
+  go () {
+    const opt = this.opt;
+    const cWidth = window.innerWidth;
+    const cHeight = window.innerHeight;
+    const center = { x: cWidth / 2, y: cHeight / 2 };
+    this.cWidth = cWidth;
+    this.cHeight = cHeight;
+
+    document.addEventListener('click', () => {
+      DeviceOrientationEvent.requestPermission()
+      .then(state => {
+        if(state === "granted"){//允许
+            console.error("用户允许",state)
+        }else if(state === "denied"){//拒绝
+            console.error("用户拒绝",state)
+        }else if(state === "prompt"){
+            console.error("用户干了啥",state)
+        }
+      }).catch(function(err){
+          alert('error: ' + err);
+      });
+    })
+
+    if( window.DeviceOrientationEvent ){
+
+      // 在135°的旋转过程中移动的距离
+      let wratio = cWidth / 120
+      let hratio = cHeight / 135
+      let orientation = window.orientation
+      window.onorientationchange = function(){
+        // window.location.reload(true); //刷新操作
+        switch(window.orientation){
+          case -90:
+          case 90:
+              // alert("横屏:" + window.orientation);
+          break;
+          case 0:
+          case 180:
+              // alert("竖屏:" + window.orientation);
+          break;
+        }
+        orientation = window.orientation
+      }
+      window.addEventListener('deviceorientation', (e) => {
+        const alpha = e.alpha // 0 ~ 360 z
+        const beta = e.beta //-180 ~ 180 x
+        const gamma = e.gamma //  -90 ~ 90 y
+
+        let clientY = 0;
+        let clientX = 0;
+
+        if (orientation === 0) {
+          // gamma 45 ~ -45
+        } else if (orientation === 90) {
+          // beta -60 ~ 60
+          clientX = Math.round(wratio * (beta + 60))
+          // gamma 45 ~ -90
+          clientY = Math.round(hratio * (gamma + 90))
+        } else if (orientation === -90) {
+          // beta 60 ~ -60
+          clientX = Math.round(wratio * Math.abs(beta - 60))
+          // gamma -45 ~ 90
+          clientY = Math.round(hratio * Math.abs(gamma - 90))
+        } else {
+          // gamma -45 ~ 45
+        }
+
+        const diffX = clientX - center.x;
+        const diffY = clientY - center.y;
+
+        if (opt.debug) {
+          document.getElementById('debuger').innerHTML = `
+            alpha: ${ Math.round(alpha) };
+            beta: ${ Math.round(beta) };
+            gamma: ${ Math.round(gamma) };
+            orientation: ${ orientation };
+            clientY: ${ clientY };
+            diffY: ${ diffY };
+          `
+        }
+        this.move(diffX, diffY)
+      })
+    }
+    
+    document.addEventListener('mousemove', (e) => {
+      const clientX = e.clientX
+      const clientY = e.clientY
+      const diffX = clientX - center.x 
+      const diffY = clientY - center.y
+      this.move(diffX, diffY)
+    })
+  }
+
+  move (diffX, diffY) {
+    const opt = this.opt;
+    const layers = this.layers;
+    const cWidth = this.cWidth;
+    const cHeight = this.cHeight;
+    const range = { hor: { max: cWidth / 2, min: cWidth / 2 }, ver: { max: cHeight / 2, min: cHeight / 2 }};
+    const offsetX = (cWidth * opt.scale - cWidth) / 2;
+    const offsetY = (cHeight * opt.scale - cHeight)  / 2;
+    let limit = { t: offsetY, l: offsetX, b: offsetY, r: offsetX };
+
+    let translateX = 0;
+    let translateY = 0;
+    if (diffX > 0) {
+      // 焦点右移
+      translateX = diffX / range.hor.max * limit.l
+    } else {
+      // 焦点左移
+      translateX = diffX / range.hor.min * limit.r
+    }
+    if (diffY > 0) {
+      // 焦点下移
+      translateY = diffY / range.hor.max * limit.t
+    } else {
+      // 焦点上移
+      translateY = diffY / range.hor.min * limit.b
+    }
+    layers.forEach(l => {
+      let translate = `translate(${ translateX * (1 + (opt.perspective * (1 - l.dataset.depth))) }px, ${ translateY * (1 + (opt.perspective * (1 - l.dataset.depth))) }px)`
+      l.style.transform = translate
+    })
+  }
+}
---
+++ views/project/ptarchitects2/static/js/kpreload.js
@@ -0,0 +1,149 @@
+/**
+ * 图片列表预加载
+ * @param {[Array]}               imgs          [图片地址数组]
+ * @param {[String]}              eleid         [等待加载元素，只接收元素ID]
+ *
+ *  API:
+ *  setCallback 设置回调函数
+ *  @param [Object,String]        attr          [当参数类型为对象时，遍历对象数据。]
+ *  @param [,Function]            value         [回调执行的函数]
+ *  {
+ *      finish      加载全部完成执行
+ *      complete    每张图片加载完成执行
+ *      fail        某张图片失败之后执行
+ *  }
+ *  回调返回值:
+ *  returnvalue {
+ *      element    图片加载中的内容遮罩层DOM对象
+ *      percentage 图片加载进度
+ *      length     目前加载完的图片数量
+ *  }
+ */
+
+(function(window,document){
+    var isFunc = function(obj){
+        return Object.prototype.toString.call(obj) === "[object Function]";
+    }
+
+    var isDom = function (obj) {
+        return typeof HTMLElement === 'object' ? obj instanceof HTMLElement : (typeof obj === 'object' && obj.nodeType === 1 && typeof obj.nodeName === 'string');
+    }
+
+    var LoadingElement = function(type, elm, ispreload){
+        var element;
+        if(type === "img" && typeof elm === 'string'){
+            element = document.createElement("img");
+            element.src = elm;
+        } else if (isDom(elm)) {
+            element = elm
+        }
+        this.element = element;
+        return this;
+    };
+
+    LoadingElement.prototype = {
+        load:function(fn){
+            var self = this;
+            self.element.dataset.preload && (self.element.src = self.element.dataset.preload);
+            self.element.onload = function () {
+                fn();
+            };
+        },
+        error:function(fn){
+            var self = this;
+            self.element.onerror = function () {
+                console.error("有图片加载出现错误，请查看图片地址是否正确。");
+                console.error("error src: " + self.src);
+                fn();
+            };
+        }
+    }
+
+    var PreLoad = function(imgs){
+        if(!imgs instanceof Array) throw Error("预加载图片必须为数组对象");
+
+        var nCount = 0, nMaxCount, oDomPreloadImgs;
+
+        var callback = this.callback = {
+            finish : null,
+            complete : null,
+            fail : null
+        };
+
+        var createImg = function(elm){
+            var oNewElm = new LoadingElement("img", elm);
+            oNewElm.load(success);
+            oNewElm.error(fail);
+        };
+
+        var complete = function(){
+            nCount++;
+            callback.complete && callback.complete({
+                length:nCount,
+                percentage:(nCount / nMaxCount).toFixed(2)
+            });
+
+            if(nCount === nMaxCount){
+                finish();
+            }
+        };
+
+        var success = function(){
+            complete();
+        };
+
+        var fail = function(elm) {
+            callback.fail && callback.fail({
+                length:nCount,
+                percentage:(nCount / nMaxCount).toFixed(2)
+            });
+            complete();
+        };
+
+        var finish = function(){
+            callback.finish && callback.finish({
+                length:nCount,
+                percentage:(nCount / nMaxCount).toFixed(2)
+            });
+        };
+
+        this.getCallback = function(){
+            console.log(callback);
+        }
+        this.start = function(){
+            var arrImgs = imgs || [];
+            oDomPreloadImgs = document.querySelectorAll('img[data-preload]');
+            nMaxCount = arrImgs.length + oDomPreloadImgs.length;
+
+            for(var i = 0, len = arrImgs.length; i < len; i++){
+                createImg(arrImgs[i]);
+            }
+            for (var i = 0, len = oDomPreloadImgs.length; i < len; i++) {
+                createImg(oDomPreloadImgs[i])
+            }
+        };
+    };
+    PreLoad.prototype = {
+        setCallback:function(attr, value){
+            if(typeof attr === "object"){
+                for(var prop in attr){
+                    if(!isFunc(attr[prop])){
+                        console.error("callback "+ prop +" is not function");
+                        return;
+                    }
+                    this.callback[prop] = attr[prop];
+                }
+            }else if(typeof attr === "string"){
+                if(!isFunc(value)){
+                    console.error("callback "+ attr +" is not function");
+                    return;
+                }
+                this.callback[attr] = value;
+            }
+            return this;
+        }
+    };
+
+    window.PreLoad = PreLoad;
+
+}(window,document));
