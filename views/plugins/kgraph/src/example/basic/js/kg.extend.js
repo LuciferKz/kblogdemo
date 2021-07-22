4c8f34faabf5c340a89c1c9febc88c4868b27162
Kim.Zhang <724292882@qq.com>
2021-07-22 14:37:48 +0800

    feat nodeCreateEvent

--- views/plugins/kgraph/src/example/basic/js/kg.extend.js
+++ views/plugins/kgraph/src/example/basic/js/kg.extend.js
@@ -1,5 +1,7 @@
 import Util from '@/util'
 import { shapes } from './kg.config'
+import $k from '@/util/dom'
+import newElement from '@/util/dom/new-element'
 
 /**
  * 
@@ -285,6 +287,122 @@ export const edgeHoverCursor = function (edge) {
   })
   edge.on('mouseleave', function (e) {
     container.css('cursor', 'auto')
+  })
+}
+
+/**
+ * 
+ * 实现拖拽入场新增节点
+ * 
+ */
+
+const createDragNode = function(x, y) {
+  let width = 60
+  let height = 60
+  let dragNode = newElement({
+    tag: 'div',
+    style: {
+      width: width + 'px',
+      height: height + 'px',
+      border: '1px dashed #333',
+      position: 'absolute',
+      left: x - width / 2 + 'px',
+      top: y - height / 2 + 'px',
+      zIndex: 9999,
+      transform: 'translate(0, 0)'
+    }
+  })
+  $k('body').append(dragNode)
+  return dragNode
+}
+
+export const nodeCreateEvent = function(item, dom, graph) {
+  let downPoint = {}
+  let grabing = false
+  let enter = false
+  let dragNode
+  let box = []
+  let ratio = 1
+  let pagePoint = {}
+
+  let drag = function(e) {
+    let clientX = e.clientX
+    let clientY = e.clientY
+    let pageX = e.pageX
+    let pageY = e.pageY
+
+    if (!grabing) {
+      if (
+        Math.abs(downPoint.x - clientX) > 10 ||
+        Math.abs(downPoint.y - clientY) > 10
+      ) {
+        grabing = true
+        // 画布外落点处新建 pageX和pageY
+        dragNode = createDragNode(pagePoint.x, pagePoint.y)
+        $k('body').css('cursor', 'move')
+      } else {
+        return false
+      }
+    }
+
+    enter = clientX > box.l && clientX < box.r && clientY > box.t && clientY < box.b
+    let scale = enter ? ratio : 1
+    if (enter) {
+      graph.$event.trigger(e)
+    }
+    const translateX = clientX - downPoint.x
+    const translateY = clientY - downPoint.y
+    dragNode.css({
+      transform: `translate(${ translateX }px, ${ translateY }px) scale(${ scale })`
+    })
+  }
+
+  let drop = function(e) {
+    if (enter) {
+      const point = graph.getPointByClient(e.clientX, e.clientY)
+      item.x = point.x
+      item.y = point.y
+      const cfg = Object.assign({}, {
+        cfgKey: item.cfgKey,
+        props: item.props,
+        shape: item.shape,
+        label: item.label,
+        isShowLabel: item.isShowLabel,
+        vue: item.vue,
+      }, { x: point.x, y: point.y })
+
+      if (item.cfgKey === 'image') {
+        cfg.shape = { img: item.icon.dom }
+      }
+      const newNode = graph.addItem('node', cfg)
+      const targetMap = graph.get('targetMap')
+      const mouseenter = targetMap.mouseenter
+      if (mouseenter) newNode.emit('drop', { origin: e, clientX: point.x, clientY: point.y, target: mouseenter })
+      graph.saveData()
+    }
+    $k('body').css('cursor', 'auto')
+    if (dragNode) {
+      dragNode.remove()
+      dragNode = null
+    }
+    grabing = false
+    document.removeEventListener('mousemove', drag)
+    document.removeEventListener('mouseup', drop)
+  }
+
+  dom.on('mousedown', e => {
+    if (e.which === 1) {
+      downPoint.x = e.clientX
+      downPoint.y = e.clientY
+
+      box = graph.get('canvas').getBox()
+      ratio = graph.get('ratio')
+      pagePoint.x = e.pageX
+      pagePoint.y = e.pageY
+
+      document.addEventListener('mousemove', drag)
+      document.addEventListener('mouseup', drop)
+    }
   })
 }
 
--- views/plugins/kgraph/src/example/basic/modules/sidebar.js
+++ views/plugins/kgraph/src/example/basic/modules/sidebar.js
@@ -1,5 +1,6 @@
 import $k from '@/util/dom'
 import newElement from '@/util/dom/new-element'
+import { nodeCreateEvent } from '../js/kg.extend.js'
 
 const Sidebar = function(selector, graph, refs = {}) {
   let container = $k(selector)
@@ -94,127 +95,10 @@ const Sidebar = function(selector, graph, refs = {}) {
       refs
     )
     container.append(sectionItem)
-    props.key && addNodeEvt(item, sectionItem)
+    props.key && nodeCreateEvent(item, sectionItem, graph)
     item.dom = sectionItem
     item.icon = item.cfgKey == 'image' ? refs.iconimg : refs.icontext.text();
     nodes.list.push(item)
-  }
-
-  let addNodeEvt = function(item, dom) {
-    let downPoint = {}
-    let grabing = false
-    let enter = false
-    let dragNode
-    let box = []
-    let ratio = 1
-    let pagePoint = {}
-
-    let drag = function(e) {
-      let clientX = e.clientX
-      let clientY = e.clientY
-      let pageX = e.pageX
-      let pageY = e.pageY
-
-      if (!grabing) {
-        if (
-          Math.abs(downPoint.x - clientX) > 10 ||
-          Math.abs(downPoint.y - clientY) > 10
-        ) {
-          grabing = true
-          // 画布外落点处新建 pageX和pageY
-          dragNode = createDragNode(item, pagePoint.x, pagePoint.y)
-          $k('body').css('cursor', 'move')
-        } else {
-          return false
-        }
-      }
-
-      enter = clientX > box.l && clientX < box.r && clientY > box.t && clientY < box.b
-      let scale = enter ? ratio : 1
-      if (enter) {
-        graph.$event.trigger(e)
-      }
-      const translateX = clientX - downPoint.x
-      const translateY = clientY - downPoint.y
-      dragNode.css({
-        transform:
-          'translate(' +
-          translateX +
-          'px, ' +
-          translateY +
-          'px) scale(' +
-          scale +
-          ')'
-      })
-    }
-
-    let drop = function(e) {
-      if (enter) {
-        const point = graph.getPointByClient(e.clientX, e.clientY)
-        item.x = point.x
-        item.y = point.y
-        const cfg = Object.assign({}, {
-          cfgKey: item.cfgKey,
-          props: item.props,
-          shape: item.shape,
-          label: item.label,
-          isShowLabel: item.isShowLabel,
-          vue: item.vue,
-        }, { x: point.x, y: point.y })
-
-        if (item.cfgKey === 'image') {
-          cfg.shape = { img: item.icon.dom }
-        }
-        const newNode = graph.addItem('node', cfg)
-        const targetMap = graph.get('targetMap')
-        const mouseenter = targetMap.mouseenter
-        if (mouseenter) newNode.emit('drop', { origin: e, clientX: point.x, clientY: point.y, target: mouseenter })
-        graph.saveData()
-      }
-      $k('body').css('cursor', 'auto')
-      if (dragNode) {
-        dragNode.remove()
-        dragNode = null
-      }
-      grabing = false
-      document.removeEventListener('mousemove', drag)
-      document.removeEventListener('mouseup', drop)
-    }
-
-    dom.on('mousedown', e => {
-      if (e.which === 1) {
-        downPoint.x = e.clientX
-        downPoint.y = e.clientY
-
-        box = graph.get('canvas').getBox()
-        ratio = graph.get('ratio')
-        pagePoint.x = e.pageX
-        pagePoint.y = e.pageY
-
-        document.addEventListener('mousemove', drag)
-        document.addEventListener('mouseup', drop)
-      }
-    })
-  }
-
-  let createDragNode = function(item, x, y) {
-    let width = 60
-    let height = 60
-    let dragNode = newElement({
-      tag: 'div',
-      style: {
-        width: width + 'px',
-        height: height + 'px',
-        border: '1px dashed #333',
-        position: 'absolute',
-        left: x - width / 2 + 'px',
-        top: y - height / 2 + 'px',
-        zIndex: 9999,
-        transform: 'translate(0, 0)'
-      }
-    })
-    $k('body').append(dragNode)
-    return dragNode
   }
 
   let filter = function(val) {
